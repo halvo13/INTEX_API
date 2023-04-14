b4db121140ef0641d6315247578102cbd3c46e25
jhalver6 <jhalver6@byu.edu>
2023-04-14 08:50:19 -0600

    just post

--- app.py
+++ app.py
@@ -16,23 +16,23 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/predict")
-def predict(data1: int, data2: int, data3: int, data4: int, data5: int, data6: int,
-            data7: int, data8: int, data9: int, data10: int, data11: int, data12: int,
-            data13: int, data14: int, data15: int):
-    # Load model from .pkl file
-    with open('./dc_model.pkl','rb') as file:
-        model = pickle.load(file)
-        # Convert input data to DataFrame
-        df = pd.DataFrame({'data1': [data1], 'data2': [data2], 'data3': [data3],
-                            'data4': [data4], 'data5': [data5], 'data6': [data6],
-                              'data7': [data7], 'data8': [data8], 'data9': [data9],
-                               'data10': [data10], 'data11': [data11], 'data12': [data12],
-                                 'data13': [data13], 'data14': [data14], 'data15': [data15]})
-        # Make prediction
-        prediction = model.predict(df)
-        # Return Prediction as JSON response
-        return {'prediction': prediction[0]}
+# @app.get("/predict")
+# def predict(data1: int, data2: int, data3: int, data4: int, data5: int, data6: int,
+#             data7: int, data8: int, data9: int, data10: int, data11: int, data12: int,
+#             data13: int, data14: int, data15: int):
+#     # Load model from .pkl file
+#     with open('./dc_model.pkl','rb') as file:
+#         model = pickle.load(file)
+#         # Convert input data to DataFrame
+#         df = pd.DataFrame({'data1': [data1], 'data2': [data2], 'data3': [data3],
+#                             'data4': [data4], 'data5': [data5], 'data6': [data6],
+#                               'data7': [data7], 'data8': [data8], 'data9': [data9],
+#                                'data10': [data10], 'data11': [data11], 'data12': [data12],
+#                                  'data13': [data13], 'data14': [data14], 'data15': [data15]})
+#         # Make prediction
+#         prediction = model.predict(df)
+#         # Return Prediction as JSON response
+#         return {'prediction': prediction[0]}
 
 # Define endpoint for making predictions
 @app.post('/predict/post')
